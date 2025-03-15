9e24ada524c9e0a44bba33285b56151b838c0fae
kurth4cker <kurth4cker@gmail.com>
2025-03-15 04:44:43 +0300

    grpcserver: separate Curse structure

--- adapters/grpcserver/server.go
+++ adapters/grpcserver/server.go
@@ -14,8 +14,8 @@ type GreetServer struct {
 	UnimplementedGreeterServer
 }
 
-func (g GreetServer) Curse(ctx context.Context, request *GreetRequest) (*GreetReply, error) {
-	return &GreetReply{
+func (g GreetServer) Curse(ctx context.Context, request *CurseRequest) (*CurseReply, error) {
+	return &CurseReply{
 		Message: interactions.Curse(request.Name),
 	}, nil
 }
